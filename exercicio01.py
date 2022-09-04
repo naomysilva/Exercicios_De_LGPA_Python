4c14c68b3cc951dbd90555d356b1e0f9a13bd18b
naomysilva <naomyleticia92@gmail.com>
2022-09-04 13:50:52 -0300

    exercicio01

---
+++ exercicio01.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+print("-----Hello people-----")
+print() #pula linha
+numberOne = int(input("Enter a number One:"))
+numberTwo = int(input("Enter a number Two:"))
+
+print("the sum of number", numberOne, "and number", numberTwo ,"is",numberOne + numberTwo)
